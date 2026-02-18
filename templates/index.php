2597af2b42cbb0ba149321369d5282a50bb57f1e
Marek Michalik <marekmichalik43@gmail.com>
2026-02-18 16:53:01 +0100

    Fix title

--- templates/index.php
+++ templates/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>{{ name }}</title>
+    <title><?php echo $name; ?></title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.classless.min.css">
 </head>
 <body>
